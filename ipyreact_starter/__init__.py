c855b896b07cfd39a2033fb0c7e81da7b2e95f73
Paddy Mullen <paddy@paddymullen.com>
2024-11-17 21:07:02 -0500

    WIP, still not able to import from the module

--- ipyreact_starter/__init__.py
+++ ipyreact_starter/__init__.py
@@ -5,8 +5,6 @@ ipyreact.define_module(
     "ipyreact-tsxlib",
     Path(__file__).parent.parent / "packages/bundled-staging/ipyreact-tsxlib.minimal.esm.js")
 
-import ipyreact
-
 # note that this import_map is already part of the default
 ipyreact.define_import_map({
     "canvas-confetti": "https://esm.sh/canvas-confetti@1.6.0",
@@ -14,14 +12,19 @@ ipyreact.define_import_map({
 
 
 class Simple(ipyreact.ValueWidget):
+    _module="ipyreact-tsxlib"
     _esm="""
     import confetti from "canvas-confetti";
     import * as React from "react";
-
+    import SampleButton from "ipyreact-tsxlib";
+    
     export default function({value, setValue}) {
-        return <button onClick={() => confetti() && setValue(value + 1)}>
+        return (<div>
+            <SampleButton label={"sample label"} />
+            <button onClick={() => confetti() && setValue(value + 1)}>
             {value || 0} times confetti
         </button>
+    </div>);
     };
     """
 
